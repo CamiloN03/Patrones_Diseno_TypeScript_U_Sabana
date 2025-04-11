fc8f14acc36ef41b04506428a6ab4a440b9deaa8
Camilo Andres Navarrete Ruiz <CamiloA.Ruiz@iq-online.com>
2025-04-10 19:15:26 -0500

    prueba de todo el despliegue

--- MicroServicios_Poc/app.js
+++ MicroServicios_Poc/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/health', (req, res) => {
-  res.send('Hola mundo desde GitHub Actions + AKS + Argo');
+  res.send('Hola mundo');
 });
 
 app.listen(port, () => {

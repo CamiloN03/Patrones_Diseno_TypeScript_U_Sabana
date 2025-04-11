5c693ff46475d02b744b96d9ce578b809239b9a5
Camilo Andres Navarrete Ruiz <CamiloA.Ruiz@iq-online.com>
2025-04-10 20:19:09 -0500

    Se ajusta para que se realice desde la rama develop

--- MicroServicios_Poc/app.js
+++ MicroServicios_Poc/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/health', (req, res) => {
-  res.send('Hola mundo con Argo');
+  res.send('Hola mundo probando configuracion rama develop');
 });
 
 app.listen(port, () => {

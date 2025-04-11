6bb0957b432c2c79c30609efee1d4ee4536053a8
Camilo Andres Navarrete Ruiz <CamiloA.Ruiz@iq-online.com>
2025-04-11 18:00:01 -0500

    Pruebas replicas

--- MicroServicios_Poc/app.js
+++ MicroServicios_Poc/app.js
@@ -3,11 +3,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/health', (req, res) => {
-  res.send('Hola desde health 3 replicas');
+  res.send('Hola desde health 2 replicas');
 });
 
 app.get('/hola', (req, res) => {
-  res.send('CI- CD argo poc');
+  res.send('argo poc CI- CD');
 });
 
 app.listen(port, () => {

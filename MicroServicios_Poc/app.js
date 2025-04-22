a63e42ef30e3e476fce621767af86f3563913520
Camilo Andres Navarrete Ruiz <CamiloA.Ruiz@iq-online.com>
2025-04-22 16:49:21 -0500

    commit para creacion del video

--- MicroServicios_Poc/app.js
+++ MicroServicios_Poc/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/health', (req, res) => {
-  res.send('Running health....');
+  res.send('Running health creacion del video....');
 });
 
 app.get('/hola', (req, res) => {
